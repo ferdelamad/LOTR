c0ad3ad86db06f9f2cc66d821b284b0618d18759
Fernando <ferdelamad@gmail.com>
2018-10-24 15:37:12 -0700

    Succesfully implemented filter books function

--- public/src/components/App.jsx
+++ public/src/components/App.jsx
@@ -6,23 +6,31 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: sampleData
+      data: sampleData,
+      filter: ""
     };
     this.handleBookClick = this.handleBookClick.bind(this);
   }
 
   handleBookClick(keywords) {
     console.log("clicked", keywords);
+    const filter = {};
+    keywords.forEach(keyword => (filter[keyword] = true));
+    this.setState({ filter }, () => console.log(this.state.filter));
   }
 
   render() {
-    const { data } = this.state;
+    const { data, filter } = this.state;
     return (
       <Fragment>
         <h1>LOTR Loading!</h1>
         <ul>
           {data.map(book => (
-            <Book bookInfo={book} handleBookClick={this.handleBookClick} />
+            <Book
+              bookInfo={book}
+              handleBookClick={this.handleBookClick}
+              filter={filter}
+            />
           ))}
         </ul>
       </Fragment>
--- public/src/components/Book.jsx
+++ public/src/components/Book.jsx
@@ -2,23 +2,33 @@ import React from "react";
 
 const Book = ({
   bookInfo: { author, title, keywords, coverUrl },
-  handleBookClick
-}) => (
-  <li onClick={() => handleBookClick(keywords)}>
-    <div>Title: {author}</div>
-    <div>Title: {title}</div>
-    <div>
-      Cover:
-      <img src={coverUrl} />
-    </div>
-    <div>
-      Keywords:
-      {keywords.map(key => (
-        <div>{key}</div>
-      ))}
-    </div>
-  </li>
-);
+  handleBookClick,
+  filter
+}) => {
+  let bold = false;
+  keywords.forEach(key => {
+    if (filter[key]) {
+      bold = true;
+    }
+  });
+  return (
+    <li onClick={() => handleBookClick(keywords)}>
+      {bold && <p>YES</p>}
+      <div>Title: {author}</div>
+      <div>Title: {title}</div>
+      <div>
+        Cover:
+        <img src={coverUrl} />
+      </div>
+      <div>
+        Keywords:
+        {keywords.map(key => (
+          <div>{key}</div>
+        ))}
+      </div>
+    </li>
+  );
+};
 
 // Book.propTypes = {
 //   bookInfo: PropTypes.object.isRequired,
