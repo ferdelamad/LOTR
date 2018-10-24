d85a5d2dceb2c204d81675156e56607d4b222bb3
Fernando <ferdelamad@gmail.com>
2018-10-24 14:20:15 -0700

    Required the sample data in the App component

--- public/src/components/App.jsx
+++ public/src/components/App.jsx
@@ -1,7 +1,15 @@
 import React, { Component, Fragment } from "react";
+import { sampleData } from "../sampleData";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: sampleData
+    };
+  }
   render() {
+    console.log("data", this.state.data);
     return (
       <Fragment>
         <h1>LOTR Loading!</h1>
---
+++ public/src/sampleData.js
@@ -0,0 +1,37 @@
+export const sampleData = [
+  {
+    title: "The Fellowship of the Rings",
+    author: "J. R. R. Tolkien",
+    coverUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/51tW-UJVfHL._SX321_BO1,204,20 3,200_.jpg",
+    keywords: ["Middle Earth", "Fellowship", "Rings", "Adventure"]
+  },
+  {
+    title: "The Two Towers",
+    author: "J. R. R. Tolkien",
+    coverUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/4123zOAwAgL.jpg",
+    keywords: ["Middle Earth", "Rings", "Adventure", "Towers"]
+  },
+  {
+    title: "Out of the Silent Planet",
+    author: "C. S. Lewis",
+    coverUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/51bWklIGxjL._SY445_QL70_.jpg",
+    keywords: ["Space", "Survival"]
+  },
+  {
+    title: "Harry Potter and the Sorcerer's Stone",
+    author: "J.K. Rowling",
+    coverUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/51qlgJ6ZojL._SY346_.jpg",
+    keywords: ["Magic", "Adventure"]
+  },
+  {
+    title: "The Martian: A Novel",
+    author: "Andy Weir",
+    coverUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/41i9zBwEU0L._SY346_.jpg",
+    keywords: ["Space", "Future"]
+  }
+];
