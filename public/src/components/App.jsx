7a89c96b795e45612a759acfb4dff9bd91f0a766
Fernando <ferdelamad@gmail.com>
2018-10-24 13:56:19 -0700

    Initialized the React work environment

---
+++ public/src/components/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component, Fragment } from "react";
+
+export default class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>LOTR Loading!</h1>
+      </Fragment>
+    );
+  }
+}
